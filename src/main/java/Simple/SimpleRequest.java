f586a02f3f9b857f563d1c3f4965438f956568d8
LonerDeni <124445115+LonerDeni@users.noreply.github.com>
2023-08-17 23:17:16 +0300

    add processing query

--- src/main/java/Simple/SimpleRequest.java
+++ src/main/java/Simple/SimpleRequest.java
@@ -1,8 +1,15 @@
 package Simple;
 
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class SimpleRequest {
@@ -10,15 +17,16 @@ public class SimpleRequest {
     private String method;
     private String path;
     private String body;
+    private static List<NameValuePair> params;
 
-    public SimpleRequest(String method, String path, String body) {
+    public SimpleRequest(String method, String path, String body, List<NameValuePair> params) {
         this.path = path;
         this.method = method;
         this.body = body;
+        this.params = params;
     }
 
     public static SimpleRequest createRequest(String request) throws IOException {
-
         final var parts = request.lines().collect(Collectors.toList());
 
         if (parts.size() < 1) {
@@ -27,14 +35,22 @@ public class SimpleRequest {
         }
         String[] requestLineParts = parts.get(0).split(" ");
 
+        List<NameValuePair> params = new ArrayList<>();
+        String path = null;
+        try {
+            URI uri = new URI(requestLineParts[1]);
+            params = URLEncodedUtils.parse(new URI(requestLineParts[1]).getQuery(), Charset.forName("UTF-8"));
+            path = uri.getPath();
+        } catch (URISyntaxException e) {
+            e.getMessage();
+        }
+
         if (requestLineParts.length == 0) {
             System.out.println("Bad request");//400
             throw new IOException("Invalid request");
         }
 
         String method = requestLineParts[0];
-        String path = requestLineParts[1];
-        List<String> headers = new ArrayList<>();
         String body;
 
         int count = 1;
@@ -48,7 +64,7 @@ public class SimpleRequest {
         }
         body = builder.toString();
 
-        return new SimpleRequest(method, path, body);
+        return new SimpleRequest(method, path, body, params);
     }
 
     public String getMethod() {
@@ -61,5 +77,13 @@ public class SimpleRequest {
 
     public String getBody() {
         return body;
+    }
+
+    public static Optional<NameValuePair> getQueryParam(String parameter) {
+        return params.stream().filter(x -> x.getName().equals(parameter)).findFirst();
+    }
+
+    public static List<NameValuePair> getQueryParams() {
+        return params;
     }
 }
